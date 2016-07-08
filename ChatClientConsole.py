98c226deb6119558060a84efe3175a04c202f309
Konstantin <kedimitrov@gmail.com>
2016-07-08 23:11:42 +0300

    new version

--- ChatClientConsole.py
+++ ChatClientConsole.py
@@ -1,10 +1,10 @@
-# 2 socketa - za prastane i za poluchawane
-# purvo se prasta 0 za poluchawane ili 1 za 1 izprastane ot klienta
-# sled towa se izprasta username
-# wsichki saobstebia sa po 32 byte
-# servera ima dwa spisaka sus socketi - edin za poluchawane i edin za prastane
-# pri swarzwane na socket se chete parvi byte - ako e 0, znachi klienta ste poluchawa - t.e. e za prastane
-
+""" 2 socketa - za prastane i za poluchawane
+ purvo se prasta 0 za poluchawane ili 1 za 1 izprastane ot klienta
+ sled towa se izprasta username
+ wsichki saobstebia sa po 32 byte
+ servera ima dwa spisaka sus socketi - edin za poluchawane i edin za prastane
+ pri swarzwane na socket se chete parvi byte - ako e 0, znachi klienta ste poluchawa - t.e. e za prastane
+"""
 
 import wx
 import os
@@ -13,187 +13,192 @@ import logging
 import socket
 import time
 import sys
-#import win32event
 
-EVT_RESULT_ID = wx.NewId()
 
-def EVT_RESULT(win, func):
+evt_result_id = wx.NewId()
+
+def evt_result(win, func):
     """Define Result Event."""
-    win.Connect(-1, -1, EVT_RESULT_ID, func)
-    
+    win.Connect(-1, -1, evt_result_id, func)
+
+   
 class ResultEvent(wx.PyEvent):
     """Simple event to carry arbitrary result data."""
     def __init__(self, data):
         """Init Result Event."""
         wx.PyEvent.__init__(self)
-        self.SetEventType(EVT_RESULT_ID)
+        self.SetEventType(evt_result_id)
         self.data = data
 
-def makeLength(msg, length):
+def make_length(msg, length):
     if(len(msg) > length):
         msg = msg[:length]    
     else:
         while(len(msg) < length):
             msg += ' '
     return msg
-    
+
+  
 class ChatTransfer(object):
     messages = list()
     def __init__(self):
-         self.msglock = threading.Lock() # send lock is used for sending
+         self.msg_lock = threading.Lock()
         
-    def addMessage(self, msg):
-         #with self.msglock:
-            self.messages.append(msg)
-            logging.warn("Adding message:" + msg)
-            logging.warn("Number messages: %s", len(self.messages))            
+    def add_message(self, msg):
+        self.messages.append(msg)
+        logging.warn("Adding message:" + msg)
+        logging.warn("Number messages: %s", len(self.messages))            
     
-    def getMessage(self):
-        #with self.msglock:
-            msg = self.messages.pop(0)
-            #self.messages.remove(msg)
-            return msg
+    def get_message(self):
+        msg = self.messages.pop(0)
+        return msg
     
-    def isEmpty(self):
-        #with self.msglock:
+    def is_empty(self):
             r = len(self.messages) == 0
-            logging.warn("isEmpty number msg: %s Returning %s", len(self.messages), r)   
+            logging.warn("is empty number msg: %s Returning %s", len(self.messages), r)   
             return r
 
             
 class ChatReceiveThread(threading.Thread):
-    def __init__(self, chatTransfer, receiveEvent, sock, notify_win):
+    def __init__(self, ChatTransfer, receive_event, sock, notify_win):
+        logging.warn('ChatReceiveThread init')
         super(ChatReceiveThread,self).__init__()
-        self.chatTransfer = chatTransfer
-        self.receiveEvent = receiveEvent
-        self.shouldRun = True
+        self.ChatTransfer = ChatTransfer
+        self.receive_event = receive_event
+        self.should_run = True
         self.sock = sock
         self._notify_window = notify_win
     
-    def signalEnd(self):
-        self.shouldRun = False
+    def signal_end(self):
+        self.should_run = False
     
     def run(self):
-        while(self.shouldRun):
+        logging.warn('ChatReceiveThread started, Should run:%d', self.should_run)
+        while(self.should_run):
+            logging.warn('Callign recv')
             msg = self.sock.recv(32)
+            logging.warn('Recv result:' + msg)
             if(msg.strip() == ''):
                 continue
-            #self.chatTransfer.addMessage(msg)
             logging.warn("Received msg: %s", msg)
-            self.receiveEvent.set()
+            self.receive_event.set()
             wx.PostEvent(self._notify_window, ResultEvent(msg))
         
 class ChatSendThread(threading.Thread):
-    def __init__(self, chatTransfer, sendEvent, sock):
+    def __init__(self, ChatTransfer, send_event, sock):
         super(ChatSendThread,self).__init__()
-        self.chatTransfer = chatTransfer
-        self.sendEvent = sendEvent
-        self.shouldRun = True
+        self.ChatTransfer = ChatTransfer
+        self.send_event = send_event
+        self.should_run = True
         self.sock = sock
         self.sock.setblocking(1)
        
     def run(self):
         logging.warn("ChatSendThread started")
-        while(self.shouldRun):
-            if(self.sendEvent.wait(5)):
+        while(self.should_run):
+            if(self.send_event.wait(5)):
                 logging.warn("send event fired")
                 msg = "Test Message"
-                if not self.chatTransfer.isEmpty():
-                    msg = self.chatTransfer.getMessage()
+                if not self.ChatTransfer.is_empty():
+                    msg = self.ChatTransfer.get_message()
                 logging.warn("start sending message: <%s>", msg)
                 
                 if(self.sock.sendall(msg) == None):
-                    logging.warn("Successfuly sent")
+                    logging.warn("Successfuly sent:" + msg)
                 else:
                     logging.warn("Error sending sent")
-                self.sendEvent.clear()
-            else:
-                logging.warn("send event time out")
-            
-    def signalEnd(self):
-        self.shouldRun = False
+                self.send_event.clear()
+
+    def signal_end(self):
+        self.should_run = False
         
-    def setShouldStop(self):
-        self.shouldRun = False
+    def set_should_stop(self):
+        self.should_run = False
     
 class MainWindow(wx.Frame):
     def __init__(self, parent, title, user):
-        self.dirname=''
-        self.setUser(user)
-        # A "-1" in the size parameter instructs wxWidgets to use the default size.
-        # In this case, we select 200px width and the default height.
+        self.dir_name=''
+        self.set_user(user)
         wx.Frame.__init__(self, parent, title=title, size=(1600,400))
         self.control = wx.TextCtrl(self)
-        self.chatText = wx.TextCtrl(self, style=wx.TE_READONLY | wx.TE_MULTILINE)
+        self.chat_text = wx.TextCtrl(self, style=wx.TE_READONLY | wx.TE_MULTILINE)
         
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        self.CreateStatusBar() 
 
-        # Setting up the menu.
-        filemenu= wx.Menu()
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open"," Open a file to edit")
-        menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+        file_menu= wx.Menu()
+        menu_open = file_menu.Append(wx.ID_OPEN, "&Open"," Open a file to edit")
+        menu_about= file_menu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        menu_exit = file_menu.Append(wx.ID_EXIT,"&Exit"," Terminate the program")
 
         # Creating the menubar.
-        menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+        menu_bar = wx.MenuBar()
+        menu_bar.Append(file_menu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menu_bar)  # Adding the MenuBar to the Frame content.
 
         # Events.
-        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.on_open, menu_open)
+        self.Bind(wx.EVT_MENU, self.on_exit, menu_exit)
+        self.Bind(wx.EVT_MENU, self.on_about, menu_about)
 
         self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
         self.buttons = []
         
         logging.warn("Start connecting")
-        HOST = 'localhost'    # The remote host
-        PORT = 8182              # The same port as used by the server
-        self.recvSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sendSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = 'localhost'    # The remote host
+        port = 8182              # The same port as used by the server
+        self.recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         try:
-            self.recvSock.connect((HOST, PORT))
-            self.recvSock.sendall('0')
-            self.recvSock.sendall(self.user)
+            self.recv_sock.connect((host, port))
+            self.recv_sock.sendall('0')
+            self.recv_sock.sendall(self.user)
+            logging.info("Receive Socket connected")
             
-            self.sendSock.connect((HOST, PORT))
-            self.sendSock.sendall('1')
-            self.sendSock.sendall(self.user)
+            self.send_sock.connect((host, port))
+            self.send_sock.sendall('1')
+            self.send_sock.sendall(self.user)
+            logging.info("send Socket connected")
+            
         
         except socket.error:
-            self.shouldRun = False
+            self.should_run = False
             return
         
             
         logging.warn('connected')   
         
-        self.chatTransfer = ChatTransfer()
-        self.chatTransferRecv = ChatTransfer()
-        self.sendEvent = threading.Event()
-        self.recvEvent = threading.Event()
-        self.chatLock = threading.Lock()
-        self.chatThread = ChatSendThread(self.chatTransfer, self.sendEvent, self.sendSock)
-        self.recvThread = ChatReceiveThread(self.chatTransferRecv, self.recvEvent, self.recvSock, self)
-        self.chatThread.start()
-        self.recvThread.start()
+        self.chat_transfer = ChatTransfer()
+        self.chat_transfer_recv = ChatTransfer()
+        self.send_event = threading.Event()
+        self.recv_event = threading.Event()
+        self.chat_lock = threading.Lock()
+        logging.warn("Init receive thread")
+        self.recv_thread = ChatReceiveThread(self.chat_transfer_recv,
+                                             self.recv_event,
+                                             self.recv_sock, self)
+        self.chat_thread = ChatSendThread(self.chat_transfer,
+                                          self.send_event,
+                                          self.send_sock)
+
+        self.chat_thread.start()
+        self.recv_thread.start()
         
-        #self.buttons.append(wx.Button(self, -1, "&Send")
-        #self.sizer2.Add(self.buttons[0], 1, wx.EXPAND)
         self.buttons.append(wx.Button(self, -1, "&Send"))
+        self.buttons.append(wx.Button(self, -1, "&Send File"))
+        
         self.sizer2.Add(self.buttons[0], 1, wx.EXPAND)
+        self.sizer2.Add(self.buttons[1], 1, wx.EXPAND)
+        
         self.buttons[0].SetDefault()
         
-        self.Bind(wx.EVT_BUTTON, self.OnSend, self.buttons[0])
-        EVT_RESULT(self,self.OnResult)
-        #for i in range(1, 7):
-        #    self.buttons.append(wx.Button(self, -1, "Button &"+str(i)))
-        #    self.sizer2.Add(self.buttons[i], 1, wx.EXPAND)
+        self.Bind(wx.EVT_BUTTON, self.on_send, self.buttons[0])
+        self.Bind(wx.EVT_BUTTON, self.on_send_file, self.buttons[1])
+        
+        evt_result(self,self.on_result)
 
-        # Use some sizers to see layout options
         self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(self.chatText, 1, wx.EXPAND)
+        self.sizer.Add(self.chat_text, 1, wx.EXPAND)
         self.sizer.Add(self.control, 2, wx.EXPAND)
         
         self.sizer.Add(self.sizer2, 0, wx.EXPAND)
@@ -204,60 +209,70 @@ class MainWindow(wx.Frame):
         self.sizer.Fit(self)
         self.Show()
         
-    def OnResult(self, event):
+    def on_result(self, event):
         """Show Result status."""
-        #if event.data is None:
-            # Thread aborted (using our convention of None return)
-            #self.status.SetLabel('Computation aborted')
-        #else:
-        logging.warn("OnResult: event.data: %s", event.data)
-        completeChat = self.chatText.GetValue()
-        completeChat += "\n" +  event.data
-        completeChat = completeChat[-100:]
-        self.chatText.SetValue(event.data)
-            # Process results here
-            #self.status.SetLabel('Computation Result: %s' % event.data)
-            # In either event, the worker is done
-            #self.worker = None
+       
+        logging.warn("On result: event.data: %s", event.data)
+        complete_chat = self.chat_text.GetValue()
+        complete_chat += "\n" +  event.data
+        complete_chat = complete_chat[-100:]
+        self.chat_text.SetValue(event.data)
     
-    def OnSend(self,e):
+    def on_send(self,e):
         msg = self.control.GetValue()
         logging.warn(msg)
-        self.chatTransfer.addMessage(makeLength(msg, 32))
+        self.chat_transfer.add_message(make_length('0' + msg, 32))
         self.control.Clear()
-        logging.warn("Setting send event")
-        self.sendEvent.set()
+        logging.warn("Setting send event for:" + msg)
+        self.send_event.set()
     
-    def OnAbout(self,e):
+    def on_send_file(self,e):
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        file_contents = ""
+        if dlg.ShowModal() == wx.ID_OK:
+            file_name = dlg.GetFilename()
+            dir_name = dlg.GetDirectory()
+            f = open(os.path.join(dir_name, file_name), 'r')
+            file_contents = f.read()
+            f.close()
+        dlg.Destroy()
+        file_length = len(file_contents)
+        self.chat_transfer.add_message('1' + make_length(str(file_length), 8) + make_length(file_name, 64)  + file_contents)
+        self.control.Clear()
+
+        self.send_event.set()
+    
+    def on_about(self,e):
         # Create a message dialog box
         dlg = wx.MessageDialog(self, "Chat client", "About FMI CHAT", wx.OK)
         dlg.ShowModal() # Shows it
         dlg.Destroy() # finally destroy it when finished.
 
-    def OnExit(self,e):
-        self.chatThread.setShouldStop()
+    def on_exit(self,e):
+        self.chat_thread.set_should_stop()
         self.Close(True)  # Close the frame.
 
-    def OnOpen(self,e):
+    def on_open(self,e):
         """ Open a file"""
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
-            self.filename = dlg.GetFilename()
-            self.dirname = dlg.GetDirectory()
-            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.file_name = dlg.GetFilename()
+            self.dir_name = dlg.GetDirectory()
+            f = open(os.path.join(self.dir_name, self.file_name), 'r')
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
     
-    def setUser(self, user):
-        self.user = makeLength(user, 32)
+    def set_user(self, user):
+        self.user = make_length(user, 32)
     
-#logging.config.fileConfig('logging.conf')
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.info("Info")
 logging.warn("warn")
 logging.debug("debug")
+
 if(len(sys.argv) < 2):
     logging.warn("No arguments. Usage: python ChatClientConsole <user>")
     exit()
--- ChatServer.py
+++ ChatServer.py
@@ -4,97 +4,141 @@ import logging
 import time
 #import sleep
 import select
+import os
 
-sList = dict()
-rList = dict()
+s_list = dict()
+r_list = dict()
 
 class ServerState(object):
-    sendList = dict()
-    recvList = dict()
-    sendListLock = threading.Lock()
+    #sendList = dict()
+    #recvList = dict()
+    #sendListLock = threading.Lock()
     def __init(self):
         #self.sendList = list()
         #self.sendListLock = threading.lock()
         return
         
-    def addClient(self, socket):
+    def add_client(self, socket):
+    
         type = socket.recv(1)
         user = socket.recv(32).strip()
         logging.warn("Type: %s, User: %s", type, user)
         if(type == '0'):
-            self.sendList[user] = socket
-            sList[user] = socket
+            #self.sendList[user] = socket
+            s_list[user] = socket
+            logging.warn("Added client. Current number: %d", len(s_list.keys()))
         if(type == '1'):
-            self.recvList[user] = socket
-            rList[user] = socket
+            #self.recvList[user] = socket
+            r_list[user] = socket
         #with self.sendListLock:
-        #    self.sendList.append(socket)
+            #self.sendList.append(socket)
         
         socket.setblocking(1)
     
-    def getSendList(self):
-        return dict(self.sendList)
+    #def getSendList(self):
+    #    return dict(self.sendList)
     
-    def getRecvList(self):
-        return dict(self.recvList)
+    #def getRecvList(self):
+    #    return dict(self.recvList)
     
     
-    def hassendList(self):
-        with self.sendListLock:
-            if self.sendList is None:
-                return False
-            if len(self.sendList) > 0:
-                return True
+    def has_send_list(self):
+        #with self.sendListLock:
+        logging.warn("hassendList s_list: %d", len(s_list.keys()))
+        if s_list is None:
+            return False
+        if len(s_list.keys()) > 0:
+            return True
         
         return False
 
 class ClientThread(threading.Thread):
     def __init__(self, serverState):
         super(ClientThread,self).__init__()
-        self.serverState = serverState
-        self.shouldRun = True
+        self.server_state = ServerState()
+        self.should_run = True
         return
     
-    def signalEnd(self):
-        self.shouldRun = False
+    def signal_end(self):
+        self.should_run = False
         return
     
     def run(self):
         logging.warn("Start")   
-        while self.shouldRun:
-            if not self.serverState.hassendList():
+        while self.should_run:
+            logging.warn("will call has_send_list")
+            if not self.server_state.has_send_list():
                 logging.warn("no cients, sleeping")
                 time.sleep(5)
             else:
-
-                recvList = rList # self.serverState.getRecvList()
-                sendList = sList # self.serverState.getSendList()
-                #logging.warn("Recv List: %d, Send List : %d", len(recvList), len(sendList))
-                if(len(recvList) == 0 and len(sendList) == 0):
+                logging.warn("has clients, process")
+                recv_list = r_list # self.serverState.getRecvList()
+                send_list = s_list # self.serverState.getSendList()
+                if(len(recv_list) == 0 and len(send_list) == 0):
+                    logging.warn("empty list continue")
                     continue
                 #msg = rList.values()[0].recv(32)
                 #logging.warn("Received: %s", msg)
 
                 #sList.values()[0].sendall("test")
-                ready_to_read, ready_to_write, in_error  = select.select(recvList.itervalues(), [], [], 60)
-                receivedMessages = list()
-                
-                logging.warn("Number messages to receive: %d %d %d",   len(ready_to_read), len(ready_to_write), len(in_error))
+                logging.warn("call select")
+                ready_to_read, ready_to_write, in_error = select.select(recv_list.itervalues(), [], [])
+                received_messages = list()
+                    
+                logging.warn("Number messages to receive: %d %d %d",
+                             len(ready_to_read),
+                             len(ready_to_write),
+                             len(in_error))
                 for s in ready_to_read:
+                        user_name = recv_list.keys()[recv_list.values().index(s)]
                     #if recvList.values()[i] in ready_to_read:
                 #for s in rList.values():
-                        data = s.recv(32)
-                        data = '> ' + data # append user name to message
-                        receivedMessages.append(data)
-                        logging.warn("Client received msg: %s",  data)
+                        try:
+                            type = s.recv(1)
+                            if(type == '0') : # message
+                                data = s.recv(32)
+                                data = user_name + '> ' + data # append user name to message
+                                received_messages.append(data)
+                                logging.warn("Client received msg: %s",  data)
+                            if(type == '1') : #file
+                                file_length = int(s.recv(8))
+                                file_name = s.recv(64).strip()
+                                file_contents = s.recv(file_length)
+                                directory = 'recv.files'
+                                if not os.path.exists(directory):
+                                    os.makedirs(directory)
+                                full_name = os.path.join(directory, user_name
+                                                         + '_' + file_name)
+                                if os.path.exists(full_name):
+                                    data = user_name
+                                    + """ wants send you a file named:"""
+                                    + file_name
+                                    + """. You aleady have received file
+                                    with such name from this user.
+                                    To receive new file with the same name
+                                    - please delete existing file at:"""
+                                    + full_name
+                                else:
+                                    with open(full_name, "w") as text_file:
+                                        text_file.write(file_contents)
+                                    data = user_name 
+                                    + ' have sent a file written to:'
+                                    + full_name
+                                received_messages.append(data)
+                        except socket.error:
+                            recv_list.pop(user_name, s)
+                            continue
                 
                 #ready_to_read, ready_to_write, in_error = select.select([], sockets, [], 5)
-                ready_to_read, ready_to_write, in_error  = select.select([], sendList.itervalues(), [], 60)
+                ready_to_read, ready_to_write, in_error = select.select([], send_list.itervalues(), [], [])
                 
-                logging.warn("Number messages to send: %d %d %d",  len(ready_to_read), len(ready_to_write), len(in_error))
+                logging.warn("Number messages to send: %d %d %d",
+                             len(ready_to_read),
+                             len(ready_to_write),
+                             len(in_error))
                 for s in ready_to_write:
                     #if ready_to_write[j]:
-                        for msg in receivedMessages:
+                        for msg in received_messages:
                             s.sendall(msg)
                             logging.warn("sending msg back")
                 
@@ -107,53 +151,54 @@ class ClientThread(threading.Thread):
 class AcceptringThread(threading.Thread):
     def __init__(self, port, serverState):
         super(AcceptringThread,self).__init__()
-        self.shouldRun = True
+        self.should_run = True
         self.port = port
-        self.sendList = []
-        self.serverState = serverState
+        self.send_list = []
+        self.server_state = ServerState()
         return
     
-    def signalEnd(self):
-        self.shouldRun = False
+    def signal_end(self):
+        self.should_run = False
+        logging.warn("AcceptringThread signal_end called")
         return
     
     def run(self):
-        logging.warn("Accepting thread start")
-        socket.setdefaulttimeout(60) # seconds
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #socket.setdefaulttimeout(60.0) # seconds
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #bind the socket to a public host,
         # and a well-known port
-        serversocket.bind(('localhost', self.port))
+        server_socket.bind(('localhost', self.port))
         #become a server socket
-        serversocket.listen(20) # number of chat sendList
+        server_socket.listen(20) # number of chat sendList
         logging.warn("Server socket created")
-        while self.shouldRun:
+        while self.should_run:
             try:
                 #accept connections from outside
-                (sendListocket, address) = serversocket.accept()
+                (send_list_socket, address) = server_socket.accept()
                 logging.warn("connection received")
-                #now do something with the sendListocket
-                #in this case, we'll pretend this is a threaded server
-                self.serverState.addClient(sendListocket)
+                self.server_state.add_client(send_list_socket)
             except socket.timeout:
                 logging.warn("no connection - timeout")
                 pass
         
+        logging.warn("AcceptringThread will end")
         #socket.close
         
 def start_server():
     port = 8182
     print("Starting server at port {0}", port)
    
-    serverState = ServerState()
-    logging.warning(serverState.hassendList())
-    clientThread = ClientThread(serverState)
-    acceptingThread = AcceptringThread(port, serverState)
-    acceptingThread.start()
-    clientThread.start()
+    server_state = ServerState()
+    logging.warning(server_state.has_send_list())
+    client_thread = ClientThread(server_state)
+    accepting_thread = AcceptringThread(port, server_state)
+    accepting_thread.start()
+    client_thread.start()
     
-    input =  raw_input("Press Enter to stop")
-    acceptingThread.signalEnd()
-    clientThread.signalEnd()
+    input("Press Enter to stop\n")
+    logging.warn("Signalling end\n")
+    
+    accepting_thread.signal_end()
+    client_thread.signal_end()
 
 start_server()
